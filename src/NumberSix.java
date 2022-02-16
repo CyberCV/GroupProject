412a718a4b2c2c1d5d5986fec63ddedf4b8998b0
CyberCV <99675601+CyberCV@users.noreply.github.com>
2022-02-15 21:33:48 -0500

    1 commit

---
+++ src/NumberSix.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class NumberSix {
+
+	
+		// TODO Auto-generated method stub
+		    public static void main(String args[]) { //forgot to add main
+		     Scanner scan = new Scanner(System.in);
+		     System.out.println("please enter a number");
+		     
+		     int n= scan.nextInt();
+		     
+		     if(n%2==1) {
+		    	 System.out.println("number is  prime");
+		     }else {
+		    	 System.out.println("number is not prime");
+		     }
+		    	
+		     
+		     
+		     
+		     
+		    }
+}
